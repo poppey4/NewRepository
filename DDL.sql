8dc476af2e6dd4cb7a0b107095662e2019ee57b7
poppey4 <149803926+poppey4@users.noreply.github.com>
2023-12-01 01:40:54 +0530

    DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,2 +1,10 @@
-ALTER TABLE test1.mytable ADD col6 VARCHAR(67)      ;
+CREATE SCHEMA test1;
+
+CREATE  TABLE test1.mytable ( 
+	col4                 VARCHAR(44)       ,
+	col2                 VARCHAR(66)       ,
+	col3                 VARCHAR(44)       ,
+	col1                 VARCHAR(77)       ,
+	column5              VARCHAR(87)       ,   
+ ) engine=InnoDB;
 
