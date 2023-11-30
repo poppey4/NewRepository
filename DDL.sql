875e6eb866164c7258d8ad9750e1e341f27ff6e2
Poppey4 <robinshawofficial@gmail.com>
2023-12-01 00:56:12 +0530

    Your commit message here

---
+++ DDL.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA test1;
+
+CREATE  TABLE test1.mytable ( 
+	col4                 VARCHAR(44)       ,
+	col2                 VARCHAR(66)       ,
+	col3                 VARCHAR(44)       ,
+	col1                 VARCHAR(77)       ,
+	column5              VARCHAR(87)       
+ ) engine=InnoDB;
+
